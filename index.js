b063cc6b33ef2ad15fad2d6a3f4f040cc377bead
Karanpreet Singh <karanpreetsingh817@gmail.com>
2023-04-30 00:16:22 +0530

    assignment 4

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ const card3 = document.getElementById('card3');
 const card4 = document.getElementById('card4');
 
 card1.addEventListener('click', () => {
-  window.location.href = 'p1.html';
+  window.location.href = './pages/p1.html';
 });
 
 card2.addEventListener('click', () => {
-  window.location.href = 'p2.html';
+  window.location.href = './pages/p2.html';
 });
 
 card3.addEventListener('click', () => {
-  window.location.href = 'p3.html';
+  window.location.href = './pages/p3.html';
 });
 card4.addEventListener('click', () => {
-  window.location.href = 'p4.html';
+  window.location.href = './pages/p4.html';
 });
