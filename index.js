312126ba26dba0ddabef80b8072144798a899fa5
Karanpreet Singh <karanpreetsingh817@gmail.com>
2023-04-30 00:04:05 +0530

    assignment4 created

---
+++ index.js
@@ -0,0 +1,19 @@
+const card1 = document.getElementById('card1');
+const card2 = document.getElementById('card2');
+const card3 = document.getElementById('card3');
+const card4 = document.getElementById('card4');
+
+card1.addEventListener('click', () => {
+  window.location.href = 'p1.html';
+});
+
+card2.addEventListener('click', () => {
+  window.location.href = 'p2.html';
+});
+
+card3.addEventListener('click', () => {
+  window.location.href = 'p3.html';
+});
+card4.addEventListener('click', () => {
+  window.location.href = 'p4.html';
+});
